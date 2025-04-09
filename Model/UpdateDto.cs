75ae67862fc021b9402585fb1513180ace23901e
Raghu <raghuraghav929@gmail.com>
2025-04-09 16:54:33 +0530

    Added the Update data transfer object to hold the updated data from the end user

---
+++ Model/UpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SdaemonAPIProject.Model
+{
+    public class UpdateDto
+    {
+        public string Title { get; set; }
+        [Display(Name = "Description Given")]
+        public string? Description { get; set; }
+        [Required]
+        public DateTime DueDate { get; set; }
+        [Required]
+        public bool IsComplete { get; set; }
+    }
+}
