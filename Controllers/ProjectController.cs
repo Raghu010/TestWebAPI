cb51d90bba1442d0a8452e60c792706beb3aa4fc
Raghu <raghuraghav929@gmail.com>
2025-04-09 15:33:05 +0530

    Created a ProjectController class that contains a endpoint to display all data present in the database table

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SdaemonAPIProject.Data;
+using SdaemonAPIProject.Model.Entities;
+
+namespace SdaemonAPIProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly AppDBContext context;
+        public ProjectController(AppDBContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllData()
+        {
+            var allData = context.creates.ToList();
+            return Ok(allData);
+        }
+
+        //[HttpGet]
+        //public CreateEF GetSingleData(int Id)
+        //{
+        //    return context.creates.Find(Id);
+        //}
+    }
+}
