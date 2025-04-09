ad15882ebfd01b5459fa9af34224d666bf7f0285
Raghu <raghuraghav929@gmail.com>
2025-04-09 14:36:36 +0530

    Created a Data folder that contains AppDBContext file

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace SdaemonAPIProject.Data
+{
+    public class AppDBContext : IdentityDBContext
+    {
+    }
+}
